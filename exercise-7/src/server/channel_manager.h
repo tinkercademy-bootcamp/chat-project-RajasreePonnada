8a465823a1be0ce8de8b412ae398afa2085f8fc2
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-06-01 15:38:09 +0000

    feat: create a channel_manager class for channels

---
+++ exercise-7/src/server/channel_manager.h
@@ -0,0 +1,19 @@
+#ifndef CHANNEL_MANAGER_H
+#define CHANNEL_MANAGER_H
+
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <vector>
+
+namespace tt::chat::server {
+
+class ChannelManager {
+public:
+
+private:
+};
+
+} // namespace tt::chat::server
+
+#endif // CHANNEL_MANAGER_H
