185b9b29dd57d183ddb895f66f69740e7d8762aa
Rajasree Ponnada <user@ip-172-31-25-255.ap-south-1.compute.internal>
2025-06-04 05:05:08 +0000

    doc: added header comments

--- exercise-7/src/client/chat-client.h
+++ exercise-7/src/client/chat-client.h
@@ -3,17 +3,43 @@
 
 #include <netinet/in.h>
 #include <string>
+#include <stdexcept> // For std::runtime_error
 
 namespace tt::chat::client {
 class Client {
+  // This class encapsulates the socket connection and basic communication 
 public:
+  /**
+   * Constructs a Client and attempts to connect to the specified server.
+   * @param port The port number of the server.
+   * @param server_address The IP address or hostname of the server.
+   * @throws std::runtime_error if socket creation or connection fails.
+   */
   Client(int port, const std::string &server_address);
+  /**
+   * @brief Sends a message to the connected server.
+   * @param message The message string to send.
+   * @throws std::runtime_error if sending fails or client is not connected.
+   */
   std::string send_and_receive_message(const std::string &message);
+  // Destroys the Client object, ensuring the socket is closed.
   ~Client();
 
 private:
-  int socket_;
+  int socket_ = -1;
+  /**
+   * Creates a server address structure (sockaddr_in).
+   * @param server_ip The IP address of the server.
+   * @param port The port number.
+   * @return A configured sockaddr_in structure.
+   * @throws std::runtime_error on invalid address.
+   */
   sockaddr_in create_server_address(const std::string &server_ip, int port);
+  /**
+   * @brief Connects the client's socket to the specified server address.
+   * @param server_address The sockaddr_in structure representing the server.
+   * @throws std::runtime_error if connection fails.
+   */
   void connect_to_server(int sock, sockaddr_in &server_address);
 
   static constexpr int kBufferSize = 1024;
